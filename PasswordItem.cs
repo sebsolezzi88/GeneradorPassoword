2c4caaf2088ce9666bbdadc4bc957eaad9e444db
seba <ssolezzi@gmail.com>
2025-09-01 19:15:24 -0300

    Creación de clase PasswordItem

---
+++ PasswordItem.cs
@@ -0,0 +1,11 @@
+namespace ClassPasswordItem
+{
+    //Clase para regresar los password almacenados 
+    class PasswordItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Pass { get; set; } = string.Empty;
+
+    }
+}
