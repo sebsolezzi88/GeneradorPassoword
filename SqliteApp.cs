6257e26fcd301705784b646856cba8fe4b39839d
seba <ssolezzi@gmail.com>
2025-09-01 18:40:08 -0300

    Creación de la función Init para crear la tabla

--- SqliteApp.cs
+++ SqliteApp.cs
@@ -1,8 +1,26 @@
 namespace SqliteApp
 {
+    using Microsoft.Data.Sqlite;
     class SqliteApp
     {
         static readonly string connectionString = "Data Source=miBase.db";
         private static string sql;
+
+        public static void Init()
+        {
+            /* Crea la tabla si no existe */
+            try
+            {
+                using SqliteConnection conn = new(connectionString);
+                conn.Open();
+                sql = "CREATE TABLE IF NOT EXIST passwords (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, pass TEXT NOT NULL)";
+                using SqliteCommand cmd = new(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqliteException)
+            {
+                Console.WriteLine("Error al crear la base de base de detaos");
+            }
+        }
     }
 }
