60d6b79155942c7a055920886087143bad27187f
seba <ssolezzi@gmail.com>
2025-09-01 22:03:29 -0300

    Creada Clase CustomFontResolver

---
+++ CustomFontResolver.cs
@@ -0,0 +1,30 @@
+using PdfSharp.Fonts;
+using System.IO;
+
+public class CustomFontResolver : IFontResolver
+{
+    private static readonly byte[] DejaVu;
+
+    static CustomFontResolver()
+    {
+        // Cargar la fuente desde el sistema
+        DejaVu = File.ReadAllBytes("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
+    }
+
+    public byte[] GetFont(string faceName)
+    {
+        if (faceName == "DejaVuSans") return DejaVu;
+        return null!;
+    }
+
+    public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+    {
+        if (familyName.Equals("DejaVu Sans", StringComparison.OrdinalIgnoreCase))
+        {
+            return new FontResolverInfo("DejaVuSans");
+        }
+
+        // fallback
+        return new FontResolverInfo("DejaVuSans");
+    }
+}
